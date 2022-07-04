e4b58a8cc6b182d2d8f8f2b0f28cca71ee95eb3c
EshtaJee <Logrus-10@yandex.ru>
2022-07-05 00:25:49 +0300

    FixCamelCase

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,13 @@ public class Main {
         System.out.println("Вас приветствует сотовая компания \"Кидала\"!");
         System.out.println();
 
-        int cash = 99; // сумма на счете
-        int check = 2100; // сумма пополнения
+        int accountAmount = 400; // сумма на счете
+        int replenishment = 6130; // сумма пополнения
 
+        int bonus = (replenishment > 1000) ? (replenishment / 100) : 0; // начисленный бонус
+        int cashTotal = accountAmount + replenishment + bonus; // итоговая сумма
 
-//       int bonus = 0; // бонус
-
-//        if (check > 1_000) {
-//            bonus = check / 100; // отключил, но тоже вполне работает
-//       }
-
-
-        boolean x = (check > 1000);
-        int bonus = x ? (check / 100) : 0; // вдруг этот понравится больше...
-
-        int cash_new = cash + check + bonus; // итоговая сумма
-
-        System.out.println("На вашем счете " + (cash_new) + " рублей");
+        System.out.println("На вашем счете " + (cashTotal) + " рублей");
         System.out.println("Бонус составляет " + (bonus) + " рублей");
 
     }
